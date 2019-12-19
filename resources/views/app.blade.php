5d611e1f44b2fdeadf9e0adbedf3e0c959259654
snowberry000 <snowberry912@gmail.com>
2019-12-19 23:01:51 +0200

    added ziggy to pass routes.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,9 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
     <link href="{{ mix('/css/app.css') }}" rel="stylesheet" />
     <script src="{{ mix('/js/app.js') }}" defer></script>
+    @routes
 </head>
 
-<body>
+<body class="font-sans leading-none text-grey-darkest antialiased">
     @inertia
 </body>
 
