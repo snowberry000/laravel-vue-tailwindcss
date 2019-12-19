91339fcb4591470a7824b227910b20e0c6a2238d
snowberry000 <snowberry912@gmail.com>
2019-12-19 23:03:57 +0200

    Updating app init.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,24 +1,26 @@
-//require("./bootstrap");
-import VueMeta from "vue-meta";
-import { InertiaApp } from "@inertiajs/inertia-vue";
 import Vue from "vue";
+import VueMeta from "vue-meta";
+import PortalVue from "portal-vue";
+import { InertiaApp } from "@inertiajs/inertia-vue";
 
-//Vue.mixin({ methods: { route: window.route } });
+Vue.config.productionTip = false;
+Vue.mixin({ methods: { route: window.route } });
 Vue.use(InertiaApp);
+Vue.use(PortalVue);
 Vue.use(VueMeta);
 
-const app = document.getElementById("app");
+let app = document.getElementById("app");
+
 new Vue({
     metaInfo: {
-        title: "Loading...",
-        titleTemplate: "%s | YayMicro"
+        titleTemplate: title => (title ? `${title} - Ping CRM` : "Ping CRM")
     },
     render: h =>
         h(InertiaApp, {
             props: {
                 initialPage: JSON.parse(app.dataset.page),
                 resolveComponent: name =>
-                    import(`./Pages/${name}`).then(module => module.default)
+                    import(`@/Pages/${name}`).then(module => module.default)
             }
         })
 }).$mount(app);
