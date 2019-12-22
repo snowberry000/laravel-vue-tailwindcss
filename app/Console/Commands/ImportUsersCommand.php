5425612a66648b49062f6472f5c4460ca7024436
snowberry000 <snowberry912@gmail.com>
2019-12-22 16:12:52 +0200

    import users from external db to local.

---
+++ app/Console/Commands/ImportUsersCommand.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\User;
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+
+class ImportUsersCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'users:import';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $last_imported_user = User::orderBy('id', 'desc')->first();
+        $uid = isset($last_imported_user) ? $last_imported_user->uid : 0;
+        $users = DB::connection('db1')->table('users')
+            ->where('uid', '>', $uid)
+            ->where('roles', 3)
+            ->where('email_confirmed', true)
+            ->whereNull('delete_info')
+            ->orderBy('uid')
+            ->chunk(100, function ($users) {
+                foreach ($users as $user) {
+                    $import = new User();
+                    $import->uid = $user->uid;
+                    $import->name = $user->name;
+                    $import->email = $user->email;
+                    $import->password = 'reset';
+                    $import->created_at = new Carbon($user->registered_time);
+                    $import->email_verified_at = $user->email_confirmed ? new Carbon($user->registered_time) : null;
+                    $import->save();
+                }
+            });
+        Log::info('Succesfully ran user import script!');
+    }
+}
