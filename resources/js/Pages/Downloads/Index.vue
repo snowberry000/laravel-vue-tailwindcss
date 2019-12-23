4977cef4137f82f83df59248f09f76db159d644e
snowberry000 <snowberry912@gmail.com>
2019-12-23 21:06:42 +0200

    Start making downloads page.

---
+++ resources/js/Pages/Downloads/Index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="container">
+        Woopsie
+    </div>
+</template>
+
+<script>
+import Layout from "@/components/Layout";
+
+export default {
+    layout: Layout,
+    metaInfo: { title: "Downloads" }
+};
+</script>
