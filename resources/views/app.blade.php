d3bf0f651f0daa20b1ff4ca9722954daaa8d04e5
snowberry000 <snowberry912@gmail.com>
2020-01-01 15:32:37 +0200

    google tag manager.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,9 +7,22 @@
     <link href="{{ mix('/css/app.css') }}" rel="stylesheet" />
     <script src="{{ mix('/js/app.js') }}" defer></script>
     @routes
+    <!-- Google Tag Manager -->
+    <script>
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-PTCN6WG');
+    </script>
+    <!-- End Google Tag Manager -->
 </head>
 
 <body class="font-sans leading-none text-grey-darkest antialiased bg-gray-200">
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PTCN6WG" height="0" width="0"
+            style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
     @inertia
 </body>
 
