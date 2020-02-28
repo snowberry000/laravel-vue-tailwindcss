9811ab000f786026cdd72169845a2c33dffca4af
snowberry000 <snowberry912@gmail.com>
2020-02-28 22:29:47 +0200

    send uid on successfull login.

--- app/Http/Controllers/AuthSftpController.php
+++ app/Http/Controllers/AuthSftpController.php
@@ -10,13 +10,13 @@ class AuthSftpController extends Controller
 {
     public function index(Request $request)
     {
-        $user = DB::connection('db1')->table('users')->select('username', 'password')->where('username', $request->username)->first();
+        $user = DB::connection('db1')->table('users')->select('uid', 'username', 'password')->where('username', $request->username)->first();
         if (!$user) {
             return ['login' => false];
         }
         $check = Hash::check($request->password, $user->password);
         if ($check) {
-            return ['login' => true];
+            return ['login' => $user->uid];
         }
         return ['login' => false];
     }
