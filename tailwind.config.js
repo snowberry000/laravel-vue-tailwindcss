50ea7fc0f90b80b4808739824f6723dfd34e5979
snowberry000 <snowberry912@gmail.com>
2020-04-02 12:24:35 +0300

    add required packages for vue.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,9 @@
 module.exports = {
-  theme: {
-    extend: {}
-  },
-  variants: {},
-  plugins: []
-}
+    theme: {
+        extend: {}
+    },
+    variants: {
+        display: ["responsive", "hover", "focus", "group-hover"]
+    },
+    plugins: []
+};
