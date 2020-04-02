71fbe07a98952a890d06ab8d05cda2be850abe23
snowberry000 <snowberry912@gmail.com>
2020-04-02 12:22:12 +0300

    create migrations to acomodate stuff.

---
+++ database/migrations/2020_04_01_084654_create_media_videos_table.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMediaVideosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::connection('media')->create('videos', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('file_uuid', 32)->unique();
+            $table->unsignedInteger('uid');
+            $table->string('extension', 4);
+            $table->string('original_name');
+            $table->text('title')->nullable();
+            $table->longText('description')->nullable();
+            $table->text('keywords')->nullable();
+            $table->boolean('people')->nullable();
+            $table->unsignedInteger('num_people')->nullable();
+            $table->boolean('editorial')->nullable();
+            $table->string('codec')->nullable();
+            $table->unsignedInteger('width')->nullable();
+            $table->unsignedInteger('height')->nullable();
+            $table->double('framerate')->nullable();
+            $table->unsignedInteger('duration')->nullable();
+            $table->unsignedInteger('size')->nullable();
+            $table->boolean('4k')->nullable();
+            $table->tinyInteger('status')->default(0);
+            $table->timestamp('uploaded_at')->nullable();
+            $table->timestamp('published_at')->nullable();
+            $table->timestamp('updated_at')->nullable();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::connection('media')->dropIfExists('videos');
+    }
+}
---
+++ database/migrations/2020_04_01_191428_dbmedia_create_video_release.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class DbmediaCreateVideoRelease extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::connection('media')->create('releases', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('file_uuid', 32)->unique();
+            $table->bigInteger('uid');
+            $table->string('name');
+            $table->string('filename');
+            $table->string('extension');
+            $table->timestamps();
+            $table->unique(['uid', 'filename']);
+        });
+
+        Schema::connection('media')->create('release_video', function (Blueprint $table) {
+            $table->unsignedBigInteger('release_id');
+            $table->unsignedBigInteger('video_id');
+            $table->unique(['release_id', 'video_id']);
+            $table->foreign('release_id')->references('id')->on('releases')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('video_id')->references('id')->on('videos')->onDelete('cascade')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::connection('media')->table('release_video', function (Blueprint $table) {
+            $table->dropForeign(['release_id']);
+            $table->dropForeign(['video_id']);
+        });
+        Schema::connection('media')->dropIfExists('release_video');
+        Schema::connection('media')->dropIfExists('releases');
+    }
+}
