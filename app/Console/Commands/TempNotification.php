a3140ca4b9c85c77c8cfa2ca06d8ed9b90c2282a
snowberry000 <snowberry912@gmail.com>
2020-04-20 16:23:17 +0300

    send correct notification.

---
+++ app/Console/Commands/TempNotification.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Notifications\ContributorVideoEnabledNotification;
+use App\User;
+use Illuminate\Console\Command;
+
+class TempNotification extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'video:notify';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $users = User::where('video', 1)->get();
+        $users->each(function ($user) {
+            $this->info("Notification sent to {$user->email}");
+            $user->notify(new ContributorVideoEnabledNotification($user));
+        });
+    }
+}
