143bdec95d9019044d0e9c0406b3640de9f9154c
snowberry000 <snowberry912@gmail.com>
2020-04-21 21:18:28 +0300

    local.

--- app/Http/Resources/KycContributorResource.php
+++ app/Http/Resources/KycContributorResource.php
@@ -16,6 +16,8 @@ class KycContributorResource extends JsonResource
     public function toArray($request)
     {
         $lastItem = $this->media()->orderBy('accepted_time', 'desc')->first();
+        $downloads = $this->downloads;
+        $payouts = $this->payouts;
         return [
             'id' => $this->id,
             'uid' => $this->uid,
@@ -25,12 +27,12 @@ class KycContributorResource extends JsonResource
             'email' => $this->email,
             'kyc_verified_at' => $this->kyc_verified_at,
             'video' => $this->video,
-            'downloads' => $this->downloads->count(),
-            'sales' => $this->downloads->sum('value') / 2 / 100,
+            'downloads' => $downloads->count(),
+            'sales' => $downloads->sum('value') / 2 / 100,
             'available' => $this->unpaidDownloads->sum('value') / 2 / 100,
-            'paidout' => $this->payouts->sum('amount') / 100,
+            'paidout' => $payouts->sum('amount') / 100,
             'pending_payouts' => $this->pendingPayouts->sum('value') / 2 / 100,
-            'last_payout' => $this->payouts->last() ? $this->payouts->last()->paid_at : null,
+            'last_payout' => $payouts->last() ? $payouts->last()->paid_at : null,
             'kycs' => $this->kycs,
             'monthsfromcreated' => $this->created_at->diffInMonths(Carbon::now()),
             // 'imagecount' => $this->media->count(),
